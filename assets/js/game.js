dc0c93f0451fa8f0830d0e2737654efdab984fc0
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-06-23 15:22:38 -0400

    robot's name, fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,12 @@
 
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+console.log("Our robot's name is " + playerName);
+
+
+
+function fight(){
+    window.alert('The fight has begun!');
+}
+
+// fight();
